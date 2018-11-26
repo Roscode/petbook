f2f09d6bb460ad23bedef9d113c4d007e95d7e45
Emily Miller-McGlone <emilymiller-mcglone@Emilys-MacBook-Pro.local>
2018-11-25 22:31:10 -0500

    Add padding to add post

--- assets/js/components/AddPost.jsx
+++ assets/js/components/AddPost.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createPost } from 'api';
 import {
-  Formik, Form, Field, ErrorMessage,
+    Formik, Form, Field, ErrorMessage,
 } from 'formik';
 import _ from 'lodash';
 import { connect } from 'react-redux';
@@ -12,20 +12,20 @@ function AddPost(state) {
     return (
         <div>
             <Formik initialValues={{ content: '', pet_id: 1 }}
-            onSubmit={(values, { setSubmitting }) => {
-                createPost(values).finally(() => setSubmitting(false));
-            }}>
-            {({ isSubmitting }) => (
-                <Form className="form create-post-form">
-                <label>
-                    Create a post!
-                    <Field className="form-control" type="text" name="content"/>
-                </label>
-                <ErrorMessage name="content" component="div"/>
-                <button type="submit" className="btn btn-secondary" disabled={isSubmitting}>Create Post!</button>
-                </Form>
-                
-            )}
+                onSubmit={(values, { setSubmitting }) => {
+                    createPost(values).finally(() => setSubmitting(false));
+                }}>
+                {({ isSubmitting }) => (
+                    <Form className="form create-post-form ml-4 mt-2">
+                        <label>
+                            Create a post!
+                    <Field className="form-control" type="text" name="content" />
+                        </label>
+                        <ErrorMessage name="content" component="div" />
+                        <button type="submit" className="btn btn-secondary" disabled={isSubmitting}>Create Post!</button>
+                    </Form>
+
+                )}
             </Formik>
             <div>
                 {newsfeed}
@@ -37,6 +37,6 @@ function AddPost(state) {
 export default connect(state => state)(AddPost);
 
 function Post(props) {
-    let {item, key} = props;
-    return <div key={key}>{item.content}</div>
+    let { item, key } = props;
+    return <div className="ml-4" key={key}>{item.content}</div>
 }
